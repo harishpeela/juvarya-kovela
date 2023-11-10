6fa6ba86c098729784b8fa179b395c17287c7cd4
sai1781 <nagasaitac143@gmail.com>
2023-11-10 12:32:21 +0530

    Resolved_navigation2

--- src/screens/bottom-tab-base/index.js
+++ src/screens/bottom-tab-base/index.js
@@ -182,7 +182,7 @@ export default BottomTabBase = ({navigation}) => {
         // For now, let's simulate a delay of 5 seconds
         await new Promise(resolve => setTimeout(resolve, timer.loaderTime));
         // After 5 seconds, hide SplashScreen and show tabs
-        setShowTabs(true);
+        setShowTabs(true);+89 
         // SplashScreen.hide();
       } catch (error) {
         console.error('Error loading tabs:', error);
