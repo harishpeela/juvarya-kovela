82704ef1fc5de673ec99f96afc6940f0a9b658b7
Vamsi.ch <vamsich.ext@deloitte.com>
2023-12-11 15:51:03 +0530

    membership page set up

--- src/screens/profile_memberShip/index.js
+++ src/screens/profile_memberShip/index.js
@@ -3,15 +3,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState, useCallback} from 'react';
 import useFocusEffect from '@react-navigation/native';
-import {View, Text, SafeAreaView, FlatList} from 'react-native';
+import {View, Text, SafeAreaView} from 'react-native';
 import {
   BackgroundImage,
   BackHeaderNew,
-  FollowersListCard3,
   Loader,
   MemberShipCard,
 } from '../../components';
-
 import {MemberShipDetails} from '../../utils/api';
 import {styles} from './styles';
 import {colors, allTexts} from '../../common';
@@ -19,31 +17,27 @@ const ProfileMembership = ({route, navigation}) => {
   const {id} = route.params || {};
   const [data, setData] = useState([]);
   const [loader, setaLoader] = useState(false);
-  // const [loader, setLoader] = useState(true);
-  const [searchedText, setSearchedText] = useState('');
-  const [followersList, setFollowersList] = useState([]);
-  const [filteredData, setFilteredData] = useState(followersList);
 
-  // const MembershipData = async () => {
-  //   setaLoader(true);
-  //   try {
-  //     let result = await MemberShipDetails(id);
-  //     console.log('res', result?.data);
-  //     if (result) {
-  //       setaLoader(false);
-  //       setData(result?.data?.memberships);
-  //     } else {
-  //       setaLoader(false);
-  //     }
-  //   } catch (error) {
-  //     console.log('error in membership details api', error);
-  //     setaLoader(false);
-  //     alert(error);
-  //   }
-  // };
-  // useEffect(() => {
-  //   MembershipData();
-  // }, []);
+  const MembershipData = async () => {
+    setaLoader(true);
+    try {
+      let result = await MemberShipDetails(id);
+      console.log('res', result?.data);
+      if (result) {
+        setaLoader(false);
+        setData(result?.data?.memberships);
+      } else {
+        setaLoader(false);
+      }
+    } catch (error) {
+      console.log('error in membership details api', error);
+      setaLoader(false);
+      alert(error);
+    }
+  };
+  useEffect(() => {
+    MembershipData();
+  }, []);
   // useFocusEffect(
   //   useCallback(() => {
   //     MembershipData();
@@ -54,16 +48,14 @@ const ProfileMembership = ({route, navigation}) => {
     <SafeAreaView>
       <BackgroundImage />
       <View style={styles.mainContainer}>
-        <View style={styles.header}>
-          <BackHeaderNew
-            txt={'Members'}
-            onPress={() => navigation.goBack()}
-            // onPlusPress={() =>
-            //   navigation.navigate(allTexts.screenNames.addMembershipDetails)
-            // }
-          />
-          <Text>Join</Text>
-        </View>
+        <BackHeaderNew
+          txt={'Membership'}
+          onPress={() => navigation.goBack()}
+          isPlus
+          onPlusPress={() =>
+            navigation.navigate(allTexts.screenNames.addMembershipDetails)
+          }
+        />
         {loader ? (
           <View>
             <Loader size={'small'} color={colors.orangeColor} />
@@ -76,58 +68,13 @@ const ProfileMembership = ({route, navigation}) => {
                 data={data}
               />
             ) : (
-              <View style={styles.followersContainer}>
-                {loader ? (
-                  <Loader size={'large'} color={colors.orangeColor} />
-                ) : (
-                  <>
-                    <FollowersListCard3 />
-                  </>
-
-                  // <>
-                  //   {/* <ScrollView showsVerticalScrollIndicator={false}> */}
-                  //   {searchedText === '' && (
-                  //     <FlatList
-                  //       style={styles.list}
-                  //       data={followersList}
-                  //       contentContainerStyle={styles.flatListStyle}
-                  //       keyExtractor={(item, index) => item.user.id.toString()}
-                  //       renderItem={({ item }) => (
-                  //         <FollowersListCard2
-                  //           name={item.user.firstName}
-                  //           img={item.user.url}
-                  //           data={item.user}
-                  //           donation={item.user.donation}
-                  //         />
-                  //       )}
-                  //     />
-                  //   )}
-                  //   {/* </ScrollView> */}
-
-                  //   {/* <ScrollView style={{height: searchedText ? '85%' : 0}}> */}
-                  //   {searchedText && filteredData.length > 0 ? (
-                  //     <FlatList
-                  //       style={styles.list}
-                  //       data={filteredData}
-                  //       contentContainerStyle={styles.flatListStyle}
-                  //       keyExtractor={item => item.user.id.toString()}
-                  //       renderItem={({ item }) => (
-                  //         <FollowersListCard2
-                  //           name={item.user.firstName}
-                  //           img={item.user.url}
-                  //           data={item.user}
-                  //           donation={item.user.donation}
-                  //         />
-                  //       )}
-                  //     />
-                  //   ) : (
-                  //     <View style={styles.noDataContainer}>
-                  //       <Text style={styles.noDataText}>No Followers to Display</Text>
-                  //     </View>
-                  //   )}
-                  //   {/* </ScrollView> */}
-                  // </>
-                )}
+              <View
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginTop: '50%',
+                }}>
+                <Text> no memberships for this temple</Text>
               </View>
             )}
           </View>
