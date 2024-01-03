c19440f646c5dcb20ac8f12fd14d4b12adc57c43
PEELA HARISH <99802766+harishpeela@users.noreply.github.com>
2024-01-03 17:03:11 +0530

    Update index.js

--- src/screens/EventsScreen/index.js
+++ src/screens/EventsScreen/index.js
@@ -22,7 +22,6 @@ import {
 } from '../../components';
 import {allTexts, colors} from '../../common';
 import {EventList} from '../../utils/api';
-import { TopBarcard } from '../../components';
 import Card from '../../common/Card';
 const EventsScreen = ({ navigation }) => {
   const [loader, setLoader] = useState(false);
