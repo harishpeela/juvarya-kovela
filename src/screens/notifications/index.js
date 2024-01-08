e665362c01b855d2c4ef436bbdeeb2527e962784
AjayJuvarya <ajay.yellapu@juvarya.com>
2024-01-08 10:43:58 +0530

    notifications

--- src/screens/notifications/index.js
+++ src/screens/notifications/index.js
@@ -3,12 +3,8 @@ import React, {useEffect, useState} from 'react';
 import {View, Text, StatusBar} from 'react-native';
 import {getNotifications} from '../../utils/api';
 import {Loader, TopBarcard} from '../../components';
-import {
-  BackHeaderNew,
-  NotificationCard,
-  NewBackHeader
-} from '../../components';
-import { HeaderComponent } from '../../components/header_comp/HeaderComponent';
+import {BackHeaderNew, NotificationCard, NewBackHeader} from '../../components';
+import {HeaderComponent} from '../../components/header_comp/HeaderComponent';
 import {FlatList} from 'react-native-gesture-handler';
 import {colors} from '../../common';
 const Notifications = ({navigation}) => {
@@ -20,10 +16,12 @@ const Notifications = ({navigation}) => {
     try {
       let result = await getNotifications();
       let Data = result?.data?.customerRoles;
-      let mapping = Data?.filter(item => item)?.map(({notifications}) => ({notifications}));
+      let mapping = Data?.filter(item => item)?.map(({notifications}) => ({
+        notifications,
+      }));
       let FilteredData = mapping[0]?.notifications;
       console.log('filterd', FilteredData);
-      setNotificationData(FilteredData)
+      setNotificationData(FilteredData);
       setLoader(false);
     } catch (error) {
       console.log('error in notifications', error);
@@ -35,25 +33,26 @@ const Notifications = ({navigation}) => {
     GetNotifications();
   }, []);
   return (
-    <View style={{ flex: 1}}>
-      <StatusBar backgroundColor={'#FFAB0F'} animated={true} />
+    <View style={{flex: 1}}>
       <View style={{}}>
-        <View style={{height: 80, marginTop: 10}}>
-        <TopBarcard txt={'Notifications'} cancel={true} onPress={() => navigation.goBack()} />
+        <View style={{minHeight: '15%', marginTop: 10}}>
+          <TopBarcard
+            txt={'Notifications'}
+            cancel={true}
+            onPress={() => navigation.goBack()}
+          />
         </View>
         <View style={{marginTop: 40}}>
           {loader ? (
             <Loader size={'large'} color={colors.orangeColor} />
           ) : notificationdata?.length ? (
-              <FlatList
-                keyboardShouldPersistTaps="handled"
-                data={notificationdata}
-                showsVerticalScrollIndicator={false}
-                keyExtractor={({item, index}) => index}
-                renderItem={({item, index}) => (
-                  <NotificationCard data={item} />
-                )}
-              />
+            <FlatList
+              keyboardShouldPersistTaps="handled"
+              data={notificationdata}
+              showsVerticalScrollIndicator={false}
+              keyExtractor={({item, index}) => index}
+              renderItem={({item, index}) => <NotificationCard data={item} />}
+            />
           ) : (
             <View
               style={{
@@ -61,7 +60,9 @@ const Notifications = ({navigation}) => {
                 justifyContent: 'center',
                 marginTop: '40%',
               }}>
-              <Text style={{color: 'black'}}>No notifications for this user</Text>
+              <Text style={{color: 'black'}}>
+                No notifications for this user
+              </Text>
             </View>
           )}
         </View>
