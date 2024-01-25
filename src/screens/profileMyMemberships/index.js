1a2e45594bacfeba9f56d4c3057ec076f7992b3a
Vamsi.ch <vamsich.ext@deloitte.com>
2024-01-25 19:57:40 +0530

    Data_in_memberships_and_donation

--- src/screens/profileMyMemberships/index.js
+++ src/screens/profileMyMemberships/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useState, useEffect } from 'react';
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
-import { getDonationsList, GetProfilePic } from '../../utils/api';
+import { ProfileMembershipsData} from '../../utils/api';
 import {
   BackHeaderNew,
   Donations_list_Card,
@@ -14,76 +14,41 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import { Loader } from '../../components';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import { TopBarCard2 } from '../../components/topBar1/topBarCard';
+import { ProfileMembershipCard } from '../../components/profileMembershipCard';
 
-const DonationsList = ({ navigation, route }) => {
+const ProfileMyMemberships = ({ navigation, route }) => {
   const [loader, setLoader] = useState(false);
   const [searchedText, setSearchedText] = useState('');
+  const [allUserMyMemberships, setAllUserMemberships] = useState('');
   const [filteredData, setFilteredData] = useState([]);
   const [apiData, setApiData] = useState([]);
   const [loading, setLoading] = useState(false);
   const { data } = route.params || {};
 
-  const customerProfilePic = async e => {
-    try {
-      let result = await GetProfilePic(e?.email);
-      if (result?.status === 200) {
-        let responce = { ...e, url: result?.data?.url };
-        if (responce) {
-          setApiData(array => [...array, responce]);
-          setLoader(false);
-        } else {
-          console.log('eeeeee', e);
-          setLoader(false);
-        }
-      } else {
-        console.log('e state', e);
-        setLoader(false);
-      }
-    } catch (error) {
-      console.log('error in profile pic api in donations', error);
-    } 
-  };
-
-  const DonationListApi = async () => {
+  const MyMembershipsData = async () => {
     setLoader(true);
-    try {
-      let id = data?.jtProfile;
-      let result = await getDonationsList(id, 0, 60);
-      let donationDTO = result?.data?.data;
-      if (donationDTO) {
-        donationDTO.map(e => {
-          customerProfilePic(e);
-        });
-      } else {
-        setLoader(false);
-      }
-    } catch (error) {
-      console.log('error in donations list api', error);
+    let result = await ProfileMembershipsData();
+    console.log('result.date ====kkknn>', result?.data);
+    if (result) {
+      setAllUserMemberships(result?.data);
+      setLoader(false);
+    } else {
+      setAllUserMemberships([]);
       setLoader(false);
     }
   };
-
-  const handleSearch = query => {
-    setLoading(true);
-    const filteredUserData = apiData?.filter(item =>
-      item?.name?.toLowerCase().includes(query.toLowerCase()),
-    );
-    setFilteredData(filteredUserData);
-    setTimeout(() => {
-      setLoading(false);
-    }, 500);
-  };
-
   useEffect(() => {
-    DonationListApi();
+    MyMembershipsData();
   }, []);
+  console.log('Donation Data====>', allUserMyMemberships);
+
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View>
         <View style={{ minHeight: '15%', marginTop: '3%' }}>
           <TopBarCard2
-            txt={'User Donations'}
+            txt={'User Memberships'}
             back={true}
             navigation={navigation}
             navMenu={navigation}>
@@ -96,12 +61,21 @@ const DonationsList = ({ navigation, route }) => {
         <View style={styles.followersContainer}>
           {loader ? (
             <Loader size={'large'} color={colors.orangeColor} />
-          ) : searchedText === '' && apiData.length > 0 ? (
-            <Donations_list_Card data={apiData} navigation={navigation} />
+          ) : searchedText === '' && allUserMyMemberships.length > 0 ? (
+            <FlatList
+                data={allUserMyMemberships}
+                showsVerticalScrollIndicator={false}
+                keyboardShouldPersistTaps={'handled'}
+                keyExtractor={({ item, index }) => index}
+                style={{}}
+                renderItem={({ item, index }) => (
+                  <ProfileMembershipCard data={item} navigation={navigation} />
+                )}
+              />
           ) : (
             <View style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: '50%' }}>
               <Text style={{ color: colors.orangeColor, fontSize: 15, fontFamily: 'Poppins-Medium' }}>
-                No donations to display
+                No Memberships to display
               </Text>
             </View>
           )}
@@ -122,4 +96,4 @@ const DonationsList = ({ navigation, route }) => {
   );
 };
 
-export default DonationsList;
+export default ProfileMyMemberships;
