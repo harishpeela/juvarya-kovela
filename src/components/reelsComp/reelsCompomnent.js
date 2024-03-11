a616092085e73ae49ded6d8ddd72170ff0561382
TIrumala <tirumal.chatharajupalli@juvarya.com>
2024-03-11 17:13:39 +0530

    changes a=done partially

--- src/components/reelsComp/reelsCompomnent.js
+++ src/components/reelsComp/reelsCompomnent.js
@@ -1,102 +1,72 @@
-
-// import React, {useState} from 'react';
+// import React, { useState, useEffect, useCallback, useMemo } from 'react';
 // import SwiperFlatList from 'react-native-swiper-flatlist';
-// // import { videoData } from './Database';
 // import SingleReel from './singleReel';
-// const ReelsComponent = ({videoData}) => {
+
+// const ReelsComponent = ({ videoData }) => {
+//   console.log('video data', videoData?.length, videoData);
+
 //   const [currentIndex, setCurrentIndex] = useState(0);
 //   const [mute, setMute] = useState(false);
-//   const handleChangeIndexValue = ({index}) => {
+//   const [nextVideoIndex, setNextVideoIndex] = useState(1);
+
+//   const nextVideoUrl = useMemo(() => {
+//     if (nextVideoIndex < videoData?.length) {
+//       const url = videoData[nextVideoIndex]?.mediaList ? videoData[nextVideoIndex]?.mediaList[0]?.url : '';
+//       return url || null;
+//     }
+//     return null;
+//   }, [nextVideoIndex, videoData, currentIndex]);
+
+//   console.log('nextVideoUrl---->', nextVideoUrl)
+//   const handleChangeIndexValue = useCallback(({ index }) => {
 //     setCurrentIndex(index);
+//     setNextVideoIndex(index + 1);
+//   }, []);
+//   const renderItem = ({ item, index }) => {
+//     console.log('item, index----->', currentIndex, index);
+//     return videoData && videoData?.length === 1 && item && item?.mediaList ? (
+//       <SingleReel
+//         item={item}
+//         index={index}
+//         mute={mute}
+//         currentIndex={currentIndex}
+//       />
+//     ) : (
+//       <SingleReel
+//         item={item}
+//         index={index}
+//         mute={mute}
+//         currentIndex={currentIndex}
+//         nextVideoUrl={index === nextVideoIndex ? nextVideoUrl : null}
+//       />
+//     );
 //   };
 
 //   return (
-//         <SwiperFlatList
+//     <SwiperFlatList
 //       vertical={true}
 //       onChangeIndex={handleChangeIndexValue}
 //       data={videoData}
-//       renderItem={({item, index}) => (
-//         <SingleReel item={item} index={index} onPress={() =>setMute(!mute) } mute={mute} currentIndex={currentIndex} />
-//       )}
-//       keyExtractor={(item, index) => index}
+//       renderItem={renderItem}
+//       keyExtractor={(index) => index.toString()}
 //     />
 //   );
 // };
 
 // export default ReelsComponent;
 
-
-
-// import React, { useRef, useState } from 'react';
-
-// import SwiperFlatList from 'react-native-swiper-flatlist';
-
-// import SingleReel from './singleReel';
- 
-// const ReelsComponent = ({ videoData }) => {
-
-//   const swiperRef = useRef(null);
-
-//   const [autoplayIndex, setAutoplayIndex] = useState(0);
- 
-//   const handleMomentumScrollEnd = (event, state, context) => {
-
-//     if (state.index === autoplayIndex) {
-
-//       setAutoplayIndex(state.index + 1);
-
-//       swiperRef.current.scrollToIndex({ index: state.index + 1 });
-
-//     }
-
-//   };
- 
-//   return (
-
-//     <SwiperFlatList
-
-//       ref={swiperRef}
-
-//       vertical={true}
-
-//       data={videoData}
-
-//       renderItem={({ item }) => <SingleReel item={item} />}
-
-//       keyExtractor={(item, index) => index.toString()}
-
-//       index={0} 
-
-//       autoplayDelay={0}
-
-//       autoplayLoop={false} 
-
-//       autoplay={true} 
-
-//       autoplayInterval={5000} 
-
-//       onMomentumScrollEnd={handleMomentumScrollEnd}
-
-//     />
-
-//   );
-
-// };
- 
-// export default ReelsComponent;
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import SwiperFlatList from 'react-native-swiper-flatlist';
 import SingleReel from './singleReel';
 
 const ReelsComponent = ({ videoData }) => {
-  console.log('video data', videoData);
+
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const [mute, setMute] = useState(false);
   const [nextVideoIndex, setNextVideoIndex] = useState(1);
   const [nextVideoUrl, setNextVideoUrl] = useState(null);
-
+  console.log('video data', videoData);
   useEffect(() => {
     if (nextVideoIndex < videoData?.length) {
       const url = videoData[nextVideoIndex]?.mediaList ? videoData[nextVideoIndex]?.mediaList[0]?.url : '';
@@ -106,26 +76,36 @@ const ReelsComponent = ({ videoData }) => {
     }
   }, [currentIndex]);
 
-  const handleChangeIndexValue = ({ index }) => {
+  const handleChangeIndexValue = useCallback(({ index }) => {
     setCurrentIndex(index);
     setNextVideoIndex(index + 1);
+  }, []);
+
+  const renderItem = ({ item, index }) => {
+    return videoData && videoData?.length === 1 && item && item?.mediaList ? (
+      <SingleReel
+        item={item}
+        index={index}
+        mute={mute}
+        currentIndex={currentIndex}
+      />
+    ) : <>
+        <SingleReel
+          item={item}
+          index={index}
+          mute={mute}
+          currentIndex={currentIndex}
+          nextVideoUrl={videoData[nextVideoIndex]?.mediaList ? videoData[nextVideoIndex]?.mediaList[0]?.url : ''}
+        />
+    </>
   };
+
   return (
     <SwiperFlatList
       vertical={true}
       onChangeIndex={handleChangeIndexValue}
       data={videoData}
-      renderItem={({ item, index }) => (
-       item && item?.mediaList && (
-        <SingleReel
-        item={item}
-        index={index}
-        mute={mute}
-        currentIndex={currentIndex}
-        nextVideoUrl={index === nextVideoIndex ? nextVideoUrl : null}
-      />
-       )
-      )}
+      renderItem={renderItem}
       keyExtractor={(item, index) => index.toString()}
     />
   );
--- src/components/reelsComp/singleReel.js
+++ src/components/reelsComp/singleReel.js
@@ -1,287 +1,140 @@
-// import React, {useRef, useState} from 'react';
-// import {View, Text, Dimensions, TouchableOpacity, Image} from 'react-native';
-// import Video from 'react-native-video';
-// import Ionic from 'react-native-vector-icons/Ionicons';
-// import AntDesign from 'react-native-vector-icons/AntDesign';
-// import Feather from 'react-native-vector-icons/Feather';
+import React, { useRef, useEffect, useState } from 'react';
+import { View, Dimensions, ActivityIndicator, Text } from 'react-native';
+import Video from 'react-native-video';
+import Ionic from 'react-native-vector-icons/Ionicons';
+import RNFS from 'react-native-fs';
 
-// const SingleReel = ({item, index, currentIndex, onPress, mute}) => {
-//   const windowWidth = Dimensions.get('window').width;
-//   const windowHeight = Dimensions.get('window').height;
-
-//   const videoRef = useRef(null);
-
-//   const onBuffer = buffer => {
-//     console.log('buffring', buffer);
-//   };
-//   const onError = error => {
-//     console.log('error', error);
-//   };
-
-//   // const [mute, setMute] = useState(false);
-
-//   const [like, setLike] = useState(item.isLike);
-
-//   return (
-//     <View
-//       style={{
-//         width: windowWidth,
-//         height: windowHeight,
-//         position: 'relative',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//       }}>
-//       <TouchableOpacity
-//         activeOpacity={0.9}
-//         // onPress={() => setMute(!mute)}
-//         onPress={onPress}
-//         style={{
-//           width: '100%',
-//           height: '100%',
-//           position: 'absolute',
-//         }}>
-//         <Video
-//           videoRef={videoRef}
-//           onBuffer={onBuffer}
-//           onError={onError}
-//           repeat={false}
-//           resizeMode="cover"
-//           paused={currentIndex == index ? false : true}
-//           source={{uri: item?.mediaList[0]?.url}}
-//           muted={mute}
-//           seek={40}
-//           style={{
-//             width: '100%',
-//             height: '95%',
-//             position: 'absolute',
-//           }}
-//         />
-//       </TouchableOpacity>
-//       <Ionic
-//         name="volume-mute"
-//         style={{
-//           fontSize: mute ? 20 : 0,
-//           color: 'white',
-//           position: 'absolute',
-//           backgroundColor: 'rgba(52,52,52,0.6)',
-//           borderRadius: 100,
-//           padding: mute ? 20 : 0,
-//         }}
-//       />
-//       <View
-//         style={{
-//           position: 'absolute',
-//           width: windowWidth,
-//           zIndex: 1,
-//           bottom: 0, //edited
-//           padding: 10,
-//         }}>
-//         <View style={{marginBottom: '20%'}}>
-//           <TouchableOpacity style={{width: 150}}>
-//             <View
-//               style={{width: 100, flexDirection: 'row', alignItems: 'center'}}>
-//               <View
-//                 style={{
-//                   width: 32,
-//                   height: 32,
-//                   borderRadius: 100,
-//                   backgroundColor: 'white',
-//                   margin: 10,
-//                 }}>
-//                 <Image
-//                   source={{uri: item?.user?.customerProfileUrl ? item?.user?.customerProfileUrl : 'https://fanfun.s3.ap-south-1.amazonaws.com/1707819684948noimg.png'}}
-//                   style={{
-//                     width: '100%',
-//                     height: '100%',
-//                     resizeMode: 'cover',
-//                     borderRadius: 100,
-//                   }}
-//                 />
-//               </View>
-//               <Text style={{color: 'white', fontSize: 16}}>{item?.user?.firstName}</Text>
-//             </View>
-//           </TouchableOpacity>
-//           <Text style={{color: 'white', fontSize: 14, marginHorizontal: 10}}>
-//             {item?.description}
-//           </Text>
-//           {/* <View style={{flexDirection: 'row', padding: 10, marginBottom: '20%'}}>
-//             <Ionic
-//               name="ios-musical-note"
-//               style={{color: 'white', fontSize: 16}}
-//             />
-//             <Text style={{color: 'white'}}>Original Audio</Text>
-//           </View> */}
-//         </View>
-//       </View>
-//       {/* <View
-//         style={{
-//           position: 'absolute',
-//           bottom: 60, //edited
-//           right: 0,
-//         }}>
-//         <TouchableOpacity onPress={() => setLike(!like)} style={{padding: 10}}>
-//           <AntDesign
-//             name={like ? 'heart' : 'hearto'}
-//             style={{color: like ? 'red' : 'white', fontSize: 25}}
-//           />
-//           <Text style={{color: 'white'}}>{item?.likes}</Text>
-//         </TouchableOpacity>
-//         <TouchableOpacity style={{padding: 10}}>
-//           <Ionic
-//             name="ios-chatbubble-outline"
-//             style={{color: 'white', fontSize: 25}}
-//           />
-//         </TouchableOpacity>
-//         <TouchableOpacity style={{padding: 10}}>
-//           <Ionic
-//             name="paper-plane-outline"
-//             style={{color: 'white', fontSize: 25}}
-//           />
-//         </TouchableOpacity>
-//         <TouchableOpacity style={{padding: 10}}>
-//           <Feather
-//             name="more-vertical"
-//             style={{color: 'white', fontSize: 25}}
-//           />
-//         </TouchableOpacity>
-//         <View
-//           style={{
-//             width: 30,
-//             height: 30,
-//             borderRadius: 10,
-//             borderWidth: 2,
-//             borderColor: 'white',
-//             margin: 10,
-//           }}>
-//           <Image
-//             source={item?.postProfile}
-//             style={{
-//               width: '100%',
-//               height: '100%',
-//               borderRadius: 10,
-//               resizeMode: 'cover',
-//             }}
-//           />
-//         </View>
-//       </View> */}
-//     </View>
-//   );
-// };
-
-// export default SingleReel;
-
-
-
-import React, { useRef, useEffect, useState } from 'react'; 
- import { View, Dimensions, ActivityIndicator } from 'react-native'; 
- import Video from 'react-native-video'; 
- import Ionic from 'react-native-vector-icons/Ionicons'; 
- import { Video as CompressorVideo } from 'react-native-compressor'; 
-
+const SingleReel = ({ item, index, currentIndex, mute, nextVideoUrl }) => {
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+  const videoRef = useRef(null);
+  const [isBuffering, setIsBuffering] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [tempVideoPath, setTempVideoPath] = useState(null);
   
+  useEffect(() => {
+    setIsLoading(true);
+    const prefetchNextVideo = async () => {
+      if (index === currentIndex && nextVideoUrl) {
+        try {
+          const cachedVideoPath = `${RNFS.CachesDirectoryPath}/nextVideo.mp4`;
 
-const SingleReel = ({ item, index, currentIndex, mute, nextVideoUrl }) => { 
-   const windowWidth = Dimensions.get('window').width; 
-   const windowHeight = Dimensions.get('window').height; 
-   const videoRef = useRef(null); 
-   const [isBuffering, setIsBuffering] = useState(true); 
-   const [isLoading, setIsLoading] = useState(false); 
+          const options = {
+            fromUrl: nextVideoUrl,
+            toFile: cachedVideoPath,
+            background: true,
+            progressDivider: 1,
+          };
 
+          const download = RNFS.downloadFile(options);
+
+          download.promise.then(res => {
+            if (res.statusCode === 200) {
+              console.log('Download completed!----->', cachedVideoPath,res);
+              setTempVideoPath(cachedVideoPath);
+              setIsLoading(false);
+            } else {
+              console.log('Download failed!');
+              setIsLoading(false);
+            }
+          }).catch(error => {
+            console.log('Download error:', error);
+            setIsLoading(false);
+          });
+        } catch (error) {
+          console.log('Error prefetching next video:', error.message);
+          setIsLoading(false);
+        }
+      }
+    };
+    prefetchNextVideo();
+  }, [index, currentIndex, nextVideoUrl]);
   
+  const onBuffer = buffer => {
+    setIsBuffering(buffer.isBuffering);
+  };
 
-  useEffect(() => { 
-     setIsLoading(true); 
-     const prefetchNextVideo = async () => { 
-       if (currentIndex === index + 1 && nextVideoUrl) { 
-         try { 
-           const compressedUrl = await CompressorVideo.compress(nextVideoUrl, { 
-             compressionMethod: 'auto', 
-           }); 
-           setIsLoading(false);  
-           Video.prefetch(compressedUrl);  
-         } catch (error) { 
-           console.log('Error compressing and prefetching next video:', error); 
-           setIsLoading(false);  
-         } 
-       } 
-     }; 
+  const onError = error => {
+    console.log('Error loading video:', error);
+    setIsBuffering(false);
+  };
 
-  
+  const onLoad = () => {
+    setIsBuffering(false);
+  };
 
-    const bufferTimer = setTimeout(prefetchNextVideo, 10000);  
-   return () => clearTimeout(bufferTimer); 
+  return (
+    <View
+      style={{
+        width: windowWidth,
+        height: windowHeight,
+        position: 'relative',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      {isLoading && (
+        <ActivityIndicator
+          size="large"
+          color="white"
+          style={{
+            position: 'absolute',
+            zIndex: 1,
+          }}
+        />
+      )}
+      {!tempVideoPath && currentIndex === index &&
+        <>
+          <Text style={{ color: 'yellow' }}>AAAAAAAAAA</Text>
+          <Video
+            ref={videoRef}
+            onBuffer={onBuffer}
+            onError={onError}
+            onLoad={onLoad}
+            repeat={false}
+            resizeMode="cover"
+            source={{ uri: item?.mediaList[0]?.url }}
+            muted={mute}
+            style={{
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+            }}
+          />
+        </>
+      }
+      {tempVideoPath && currentIndex === index &&
+        <>
+          <Text style={{ color: 'yellow' }}>BBBBBBBBBBB</Text>
+          <Video
+            ref={videoRef}
+            onBuffer={onBuffer}
+            onError={onError}
+            onLoad={onLoad}
+            repeat={false}
+            resizeMode="cover"
+            source={{ uri: tempVideoPath }}
+            muted={mute}
+            style={{
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+            }}
+          />
+        </>
+      }
+      <Ionic
+        name="volume-mute"
+        style={{
+          fontSize: mute ? 20 : 0,
+          color: 'white',
+          position: 'absolute',
+          backgroundColor: 'rgba(52,52,52,0.6)',
+          borderRadius: 100,
+          padding: mute ? 20 : 0,
+        }}
+      />
+    </View>
+  );
+};
 
-  
-
-  }, [currentIndex]); 
-
-  const onBuffer = buffer => { 
-     setIsBuffering(buffer.isBuffering); 
-   }; 
-
-  const onError = error => { 
-     console.log('Error loading video:', error); 
-     setIsBuffering(false);  
-   }; 
-
-  const onLoad = () => { 
-     setIsBuffering(false); 
-   }; 
-  return ( 
-     <View 
-       style={{ 
-         width: windowWidth, 
-         height: windowHeight, 
-         position: 'relative', 
-         justifyContent: 'center', 
-         alignItems: 'center', 
-       }}> 
-       <Video 
-         ref={videoRef} 
-         onBuffer={onBuffer} 
-         onError={onError} 
-         onLoad={onLoad} 
-         repeat={false} 
-         resizeMode="cover" 
-         paused={currentIndex !== index} 
-         source={{ uri: item?.mediaList[0]?.url }} 
-         muted={mute} 
-         style={{ 
-           width: '100%', 
-           height: '100%', 
-           position: 'absolute', 
-         }} 
-       /> 
-       {isBuffering && !isLoading && ( 
-         <ActivityIndicator 
-           size="large" 
-           color="white" 
-           style={{ 
-             position: 'absolute', 
-             zIndex: 1, 
-           }} 
-         /> 
-       )} 
-       <Ionic 
-         name="volume-mute" 
-         style={{ 
-           fontSize: mute ? 20 : 0, 
-           color: 'white', 
-           position: 'absolute', 
-           backgroundColor: 'rgba(52,52,52,0.6)', 
-           borderRadius: 100, 
-           padding: mute ? 20 : 0, 
-         }} 
-       /> 
-     </View> 
-   ); 
- }; 
-
-  
-
-export default SingleReel; 
- 
-
- 
-
- 
+export default SingleReel;
