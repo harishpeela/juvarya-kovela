d2ffdd179378b0ad53a524a741306d8d4a14eea8
myusername <myemail>
2024-03-25 07:43:16 +0530

    histor design

--- src/screens/about-temple/index.js
+++ src/screens/about-temple/index.js
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { styles } from "./styles";
-import { TopBarCard2 } from "../../components/topBar1/topBarCard";
-import { EventInput4, Loader } from "../../components";
-import { getAboutTemple, getEditAboutTemple, saveAboutTemple } from "../../utils/api";
-import { colors } from "../../common";
-import { ScrollView } from "react-native-gesture-handler";
-import { statusBarHeight } from "../../utils/config/config";
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native';
+import { styles } from './styles';
+import { TopBarCard2 } from '../../components/topBar1/topBarCard';
+import { EventInput4, Loader } from '../../components';
+import {
+    getAboutTemple,
+    getEditAboutTemple,
+    saveAboutTemple,
+} from '../../utils/api';
+import { colors } from '../../common';
+import { ScrollView } from 'react-native-gesture-handler';
+import { statusBarHeight, windowHeight } from '../../utils/config/config';
 const AboutTemple = ({ navigation, route }) => {
     const { jtProfile, name } = route?.params || {};
     const [data, setData] = useState();
@@ -20,40 +24,74 @@ const AboutTemple = ({ navigation, route }) => {
         console.log('res of about temple', result?.data);
         if (result?.status === 200) {
             setData(result?.data);
-            setLoader(false)
+            setLoader(false);
             setDescription(result?.data?.history);
         } else {
             setData('');
             setLoader(false);
         }
-    }
+    };
 
     useEffect(() => {
         AboutTemple();
     }, []);
+    console.log(data?.data?.length)
     return (
         <View style={{ flex: 1, backgroundColor: 'white' }}>
-            <View style={{ height:60, marginTop: statusBarHeight }}>
-                <TopBarCard2 back={true} txt={'History'} navigation={navigation} marginLeft={'27%'} />
+            <View style={{ height: 60, marginTop: statusBarHeight }}>
+                <TopBarCard2
+                    back={true}
+                    txt={'History'}
+                    navigation={navigation}
+                    marginLeft={'27%'}
+                />
             </View>
-            <View>
+            <View >
                 {/* <ScrollView showsVerticalScrollIndicator={false} style={{ borderWidth: 0.5, marginHorizontal: '8%', backgroundColor: colors.white, borderRadius: 10, paddingVertical: 10 }}>
                     <Text style={{ color: 'black', margin: '4%' }}>
                         {data?.history ? data?.history : 'History To Be Added'}
                     </Text>
                 </ScrollView> */}
-                {loader ? (<>
-                    <View style={{ marginTop: '-34%' }}>
-                        <Loader size={'large'} color={colors.orangeColor} />
-                    </View>
+                {loader ? (
+                    <>
+                        <View style={{ marginTop: '-34%' }}>
+                            <Loader size={'large'} color={colors.orangeColor} />
+                        </View>
+                    </>
+                ) : (
+                    <>
+                        {data?.data.length !== 0 ? (<><ScrollView
+                            showsVerticalScrollIndicator
+                            style={{ margin: 3, padding: 10 }}>
+                            <FlatList
+                                data={data?.data}
+                                keyExtractor={(item, index) => index.toString()}
+                                renderItem={({ item }) => (
+                                    <View
+                                        style={styles.historyConatainer}>
+                                        <Image
+                                            source={{ uri: item?.jtProfileDTO?.logo }}
+                                            style={styles.TempleImage}
+                                        />
+                                        <Text
+                                            style={styles.aboutText}>
+                                            {item?.history}
+                                        </Text>
 
-                </>) : (<>
-                    <ScrollView showsVerticalScrollIndicator style={{ margin: 10, borderWidth: 0.2, padding: 12, borderRadius: 8 }}>
-                        <Text style={{ fontSize: 15, textAlign: 'justify', fontFamily: 'Poppins-Medium', color: colors.gray2 }}>{data?.history ? data?.history : 'History To Be Added'}</Text>
-                    </ScrollView>
-                </>)}
+                                    </View>
+                                )}
+                            />
+                        </ScrollView></>) : (<>
+                            <View style={{ height: windowHeight * 0.78, justifyContent: "center" }}>
+                                <Text style={{ fontSize: 18, textAlign: 'justify', fontFamily: 'Poppins-Medium', color: colors.orangeColor, alignSelf: "center", textDecorationLine: "underline" }}>
+                                    History To Be Added
+                                </Text>
+                            </View>
+                        </>)}
+                    </>
+                )}
             </View>
         </View>
-    )
-}
+    );
+};
 export default AboutTemple;
